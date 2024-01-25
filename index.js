b63aed9b08c7ea7060ae33189120c6a254678c74
AntonioEspin <antoesdev91@gmail.com>
2024-01-24 22:22:54 -0500

    ADD toggle button with themes

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const operations = document.querySelectorAll('.operation');
 const delButton = document.querySelector('.delButton');
 const resetButton = document.querySelector('.resetButton');
 const calculateButton = document.querySelector('.calculateButton');
+const toggle = document.querySelector('#toggle-range');
+
+// Elementos para el toggle
+const body = document.querySelector('body');
+const display = document.querySelector('.display span');
+const displayBackground = document.querySelector('.display');
+const keypad = document.querySelector('.container-keypad');
+const colorFont = document.querySelectorAll('.header span');
+console.log(colorFont)
+
 let displayValue = '';
 
 numbers.forEach(number => number.addEventListener('click', displayNumber));
@@ -11,6 +21,7 @@ operations.forEach(operation => operation.addEventListener('click', addOperation
 delButton.addEventListener('click', removeCharacter);
 resetButton.addEventListener('click', removeAll);
 calculateButton.addEventListener('click', calculate);
+toggle.addEventListener('input', handleToggle);
 
 
 function displayNumber () {
@@ -45,6 +56,63 @@ function removeAll () {
 }
 
 function updateDisplay () {
-  const display = document.querySelector('.display span');
   display.textContent = displayValue;
+}
+
+// Funcion del toggle
+function handleToggle () {
+  const currentValue = toggle.value
+  switch (currentValue) {
+    case '2':
+      body.style.backgroundColor = 'var(--Light-gray)';
+      colorFont.forEach(font => {
+        font.style.color = 'var(--Very-dark-grayish-yellow)';
+      })
+      toggle.style.backgroundColor = 'var(--Grayish-red)';
+      display.style.color = 'var(--Very-dark-grayish-yellow)'
+      displayBackground.style.backgroundColor = 'var(--Very-light-gray)';
+      keypad.style.backgroundColor = 'var(--Grayish-red)';
+      numbers.forEach(number => {
+        number.style.backgroundColor = 'var(--Light-grayish-yellow)';
+        number.style.boxShadow = '0px 3px 0px 0px var(--Dark-grayish-orange)';
+      })
+      operations.forEach(operation => {
+        operation.style.backgroundColor = 'var(--Light-grayish-yellow)';
+        operation.style.boxShadow = '0px 3px 0px 0px var(--Dark-grayish-orange)';
+      })
+      delButton.style.backgroundColor = 'var(--Dark-moderate-cyan)';
+      delButton.style.boxShadow = '0px 3px 0px 0px var(--Very-dark-cyan)'
+      resetButton.style.backgroundColor = 'var(--Dark-moderate-cyan)';
+      resetButton.style.boxShadow = '0px 3px 0px 0px var(--Very-dark-cyan)';
+      calculateButton.style.backgroundColor = 'var(--Orange)';
+      calculateButton.style.boxShadow = '0px 3px 0px 0px var(--Dark-orange)';
+      break;
+    case '3':
+      body.style.backgroundColor = 'var(--Very-dark-violet)';
+      colorFont.forEach(font => {
+        font.style.color = 'var(--Very-dark-grayish-yellow)';
+      })
+      toggle.style.backgroundColor = 'var(--Grayish-red)';
+      display.style.color = 'var(--Very-dark-grayish-yellow)'
+      displayBackground.style.backgroundColor = 'var(--Very-light-gray)';
+      keypad.style.backgroundColor = 'var(--Grayish-red)';
+      numbers.forEach(number => {
+        number.style.backgroundColor = 'var(--Light-grayish-yellow)';
+        number.style.boxShadow = '0px 3px 0px 0px var(--Dark-grayish-orange)';
+      })
+      operations.forEach(operation => {
+        operation.style.backgroundColor = 'var(--Light-grayish-yellow)';
+        operation.style.boxShadow = '0px 3px 0px 0px var(--Dark-grayish-orange)';
+      })
+      delButton.style.backgroundColor = 'var(--Dark-moderate-cyan)';
+      delButton.style.boxShadow = '0px 3px 0px 0px var(--Very-dark-cyan)'
+      resetButton.style.backgroundColor = 'var(--Dark-moderate-cyan)';
+      resetButton.style.boxShadow = '0px 3px 0px 0px var(--Very-dark-cyan)';
+      calculateButton.style.backgroundColor = 'var(--Orange)';
+      calculateButton.style.boxShadow = '0px 3px 0px 0px var(--Dark-orange)';
+      break;
+  
+    default:
+      break;
+  }
 }
